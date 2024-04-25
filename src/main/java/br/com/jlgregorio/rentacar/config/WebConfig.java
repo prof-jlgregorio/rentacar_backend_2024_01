7f2963f522b8dc88cd034bf8957d72bebcf6cd64
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2024-04-25 07:57:53 -0300

    CORS configuration is OK

--- src/main/java/br/com/jlgregorio/rentacar/config/WebConfig.java
+++ src/main/java/br/com/jlgregorio/rentacar/config/WebConfig.java
@@ -1,2 +1,20 @@
-package br.com.jlgregorio.rentacar.config;public class WebConfig {
+package br.com.jlgregorio.rentacar.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:8000",
+                        "http://www.jlgregorio.com.br",
+                        "http://www.fatecjales.edu.br");
+    }
 }
